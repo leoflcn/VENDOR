3d79f4a69de224a901d0e2900d42940adaec3f54
Rushilmistry <rushilmistry99@gmail.com>
2021-01-28 19:34:50 -0600

    Initial Commit

---
+++ Vendor/VendorApp.swift
@@ -0,0 +1,17 @@
+//
+//  VendorApp.swift
+//  Vendor
+//
+//  Created by Rushil Mistry on 1/28/21.
+//
+
+import SwiftUI
+
+@main
+struct VendorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
