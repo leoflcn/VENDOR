053b91d936fc14a32cab516b0c608767e3cb497e
Rushil Mistry <rushilmistry99@gmail.com>
2021-02-09 18:31:02 -0600

    New start

--- Vendor/WelcomeScreen.swift
+++ Vendor/WelcomeScreen.swift
@@ -13,27 +13,58 @@ struct WelcomeScreen: View {
             Color("UseBlack")
                 .edgesIgnoringSafeArea(.all)
             
-            Spacer()
-            
-            Button(action: {
-                print("It Works")
-            }) {
-                HStack() {
-                    Text("Log In Now")
-                        .fontWeight(.semibold)
-                        .font(.title)
+            VStack {
+                    //.frame(width: 350, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: .bottomTrailing)
+                Spacer()
+                Image("Vending Machine")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 400, height: 400)
+                    .aspectRatio(contentMode: .fit)
+                
+                Spacer()
+                
+                VStack(alignment: .leading, spacing: 1) {
+                    Text("Start to Discover")
+                        .font(.system(size: 32, weight: .medium, design: .default))
+                        .foregroundColor(.white)
+                    
+                    Text("Start finding your place right now")
+                        .font(.system(size: 20, weight: .light, design: .default))
+                        .foregroundColor(.white)
+                
                 }
-                .padding()
-                .foregroundColor(.white)
-                .background(Color.red)
-                .cornerRadius(40)
-        }
-                //Text("Start to Discover")
-                 //   .foregroundColor(Color.white)
+                
+                Spacer()
+                
+                Button(action: {
+                                print("It Works")
+                            }) {
+                                HStack() {
+                                    Text("Log In Now")
+                                        .fontWeight(.semibold)
+                                        .font(.title)
+                                }
+                                .padding()
+                                .frame(width: 390, height: 55, alignment: .center)
+                                .foregroundColor(.white)
+                                .background(Color("UseMainRed"))
+                                .cornerRadius(40)
+                            }
+                
+                Spacer()
+                
+                Text("Don's have an account?")
+                    .font(.system(size: 15, weight: .light, design: .default))
+                    .foregroundColor(.white)
+                
 
-                //Text("Start fiding your place right now")
-                    //.foregroundColor(Color.white)
+            }
         }
+        NavigationView {
+            
+        }
+        
     }
 }
 
