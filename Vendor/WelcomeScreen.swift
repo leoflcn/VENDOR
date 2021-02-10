7141f9b65bb7983c756c41fb27704e97618009ec
Rushil Mistry <rushilmistry99@gmail.com>
2021-02-09 19:10:43 -0600

    done

--- Vendor/WelcomeScreen.swift
+++ Vendor/WelcomeScreen.swift
@@ -9,62 +9,63 @@ import SwiftUI
 
 struct WelcomeScreen: View {
     var body: some View {
-        ZStack{
-            Color("UseBlack")
-                .edgesIgnoringSafeArea(.all)
-            
-            VStack {
-                    //.frame(width: 350, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: .bottomTrailing)
-                Spacer()
-                Image("Vending Machine")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 400, height: 400)
-                    .aspectRatio(contentMode: .fit)
+        NavigationView {
+            ZStack{
+                Color("UseBlack")
+                    .edgesIgnoringSafeArea(.all)
                 
-                Spacer()
-                
-                VStack(alignment: .leading, spacing: 1) {
-                    Text("Start to Discover")
-                        .font(.system(size: 32, weight: .medium, design: .default))
-                        .foregroundColor(.white)
+                VStack {
+                    Spacer()
+                    Image("Vending Machine")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 400, height: 400)
+                        .aspectRatio(contentMode: .fit)
                     
-                    Text("Start finding your place right now")
-                        .font(.system(size: 20, weight: .light, design: .default))
-                        .foregroundColor(.white)
-                
-                }
-                
-                Spacer()
-                
-                Button(action: {
-                                print("It Works")
-                            }) {
-                                HStack() {
-                                    Text("Log In Now")
-                                        .fontWeight(.semibold)
-                                        .font(.title)
+                    Spacer()
+                    
+                    VStack(alignment: .leading, spacing: 1) {
+                        Text("Start to Discover")
+                            .font(.system(size: 32, weight: .medium, design: .default))
+                            .foregroundColor(.white)
+                        
+                        Text("Start finding your place right now")
+                            .font(.system(size: 20, weight: .light, design: .default))
+                            .foregroundColor(.white)
+                    
+                    }
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                                    print("It Works")
+                                }) {
+                                    HStack() {
+                                        Text("Log In Now")
+                                            .fontWeight(.semibold)
+                                            .font(.title)
+                                    }
+                                    .padding()
+                                    .frame(width: 390, height: 55, alignment: .center)
+                                    .foregroundColor(.white)
+                                    .background(Color("UseMainRed"))
+                                    .cornerRadius(40)
                                 }
-                                .padding()
-                                .frame(width: 390, height: 55, alignment: .center)
-                                .foregroundColor(.white)
-                                .background(Color("UseMainRed"))
-                                .cornerRadius(40)
-                            }
+                    
+                    Spacer()
+                    
+                    HStack {
+                        Text("Don's have an account?")
+                            .font(.system(size: 15, weight: .light, design: .default))
+                            .foregroundColor(.white)
+                        
+                        NavigationLink(destination: Text("Registraion Page"), label: { Text("Register")})
+                            .foregroundColor(Color("Dark Red"))
                 
-                Spacer()
-                
-                Text("Don's have an account?")
-                    .font(.system(size: 15, weight: .light, design: .default))
-                    .foregroundColor(.white)
-                
-
+                    }
+                        
             }
         }
-        NavigationView {
-            
-        }
-        
     }
 }
 
@@ -74,4 +75,5 @@ struct WelcomeScreen_Previews: PreviewProvider {
             WelcomeScreen()
         }
     }
+}
 }
